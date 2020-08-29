dec31119865e89c1c2d10ad8172f744811f4a110
Brian Yee <briandidi1013@gmail.com>
2020-08-28 22:36:59 -0700

    Updates

---
+++ precache-manifest.d6a90a34e6f0549b2ff15b404ebaa5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb55cc7732b780391e1c9921a8a858f1",
+    "url": "/Aifrion/Aifrion.github.io/index.html"
+  },
+  {
+    "revision": "88683f98b071dc5dac04",
+    "url": "/Aifrion/Aifrion.github.io/static/css/main.221bbd04.chunk.css"
+  },
+  {
+    "revision": "8a3284b5c73eeb18d4fa",
+    "url": "/Aifrion/Aifrion.github.io/static/js/2.7e062505.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Aifrion/Aifrion.github.io/static/js/2.7e062505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88683f98b071dc5dac04",
+    "url": "/Aifrion/Aifrion.github.io/static/js/main.852435eb.chunk.js"
+  },
+  {
+    "revision": "a5e6a30e72182b6afde2",
+    "url": "/Aifrion/Aifrion.github.io/static/js/runtime-main.245bfbc1.js"
+  }
+]);
